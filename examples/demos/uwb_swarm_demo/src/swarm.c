d98171edf9f3dc58588ebde049cb66f47c68fce5
karlscholz <scholz.karl@hm.edu>
2022-11-22 17:44:29 +0100

    started pseudoconding swarm.c

--- examples/demos/uwb_swarm_demo/src/swarm.c
+++ examples/demos/uwb_swarm_demo/src/swarm.c
@@ -22,36 +22,73 @@
 #include "relative_localization.h"
 #include "swarmTwrTag.h"
 
+//creates swarm.c wide variable of stabilizer_type.c struct setpoint
 static setpoint_t setpoint;
+//2D-float array with all UWB Nodes in one and X, Y, YAW in the other direction
 static float rlVarForCtrl[NUM_UWB][STATE_DIM_rl];
+//Id of this dorne, based on the 40 bit radio adress, but only the last hex character(the last 4 bits) are used.
 static uint8_t myId;
+//fixed height for all operations to 0.4 m
 static float height = 0.4f;
+//variable that has to be true, otherwise a landing will be initiated within the next taskloop
 static bool keepFlying = false;
 
+/* Gives a new setpoint to the High Level Controller
+ *
+ * Parameters:
+ * - setpoint_t pointer
+ * - x Velocity as float
+ * - y velocity as float
+ * - height as float
+ * - yawrate as float
+ * Returns:
+ * - Nothing
+ *                                                                            */
 static void setHoverSetpoint(setpoint_t *setpoint, float vx, float vy, float z, float yawrate) {
+  // control loop around height
   setpoint->mode.z = modeAbs;
+  // sets z as absolute height
   setpoint->position.z = z;
+  // control loop around velocity
   setpoint->mode.yaw = modeVelocity;
+  // sets yawrate as attittude
   setpoint->attitudeRate.yaw = yawrate;
+  // control loop around velocity for x and y
   setpoint->mode.x = modeVelocity;
   setpoint->mode.y = modeVelocity;
+  // sets speed in x and y direction
   setpoint->velocity.x = vx;
   setpoint->velocity.y = vy;
+  // use airframe coordinate system instead of global coordinate system
   setpoint->velocity_body = true;
+  // give the global swarm.c struct "setpoint" by value to the High Level Commander
   commanderSetSetpoint(setpoint, 3);
 }
 
+/* Makes the CF fly 1m in a random XY direction and return 1m in the counter
+ * direction back to the starting point.
+ *
+ * Parameters:
+ * - velocity of maneuver as float
+ * Returns:
+ * - Nothing
+ *                                                                            */
 static void flyRandomIn1meter(float vel) {
   // velocity direction [0 - 2pi] rad
   float randomYaw = (rand() / (float) RAND_MAX) * 6.28f;
   // velocity magnitude [0 - 1] m
   float randomVel = vel*(rand() / (float)RAND_MAX);
+  // set x velocity = front and back velocity to x component of velocity vector
   float vxBody = randomVel * cosf(randomYaw);
+  // set y velocity = right and left velocity to y component of velocity vector
   float vyBody = randomVel * sinf(randomYaw);
+  //do the fly away maneuver for 100 * 10ms = 1s
   for (int i = 1; i < 100; i++) {
+    // change the setpoint and send it to the High Level Commander
     setHoverSetpoint(&setpoint, vxBody, vyBody, height, 0);
     vTaskDelay(M2T(10));
   }
+  // do the fly back maneuver respectively
   for (int i = 1; i < 100; i++) {
     setHoverSetpoint(&setpoint, -vxBody, -vyBody, height, 0);
     vTaskDelay(M2T(10));
@@ -59,34 +96,67 @@ static void flyRandomIn1meter(float vel) {
 }
 
 // PID control of swarm flight
+// macro SIGN returns 1 if x is positive, -1 if x is negative
 #define SIGN(a) ((a >= 0) ? 1 : -1)
+// sets the pid values 
 static float rlPIDp = 2.0f, rlPIDi = 0.0001f, rlPIDd = 0.01f;
+// creates static variables to store the Error Values of the last loop to calculate the derivative
 static float PreErrX = 0, PreErrY = 0, IntErrX = 0, IntErrY = 0;
+// creates static variable to store the timestamp of the last loop to calculate the derivative
 static uint32_t rlPIDlastTime;
+
+/* Uses a simple PID control on X and Y to move to specified location relative
+ * to leader.
+ *
+ * Parameters:
+ * - desired x position relative to leader as float
+ * - desired y position relative to leader as float
+ * Returns:
+ * - Nothing
+ *                                                                            */
 static void moveWithLeaderAsOrigin(float posX, float posY) {
+  // calculate the time difference between the last loop and this loop
   float dt = (float)(xTaskGetTickCount() - rlPIDlastTime) / configTICK_RATE_HZ;
+  // remember the timestamp of this loop
   rlPIDlastTime = xTaskGetTickCount();
+  // sanity check to just drop the calculation for one loop if counter overflow occured
   if (dt > 1)
     return;
+  // calculate setpoint error of the position
   float errX = -(posX - rlVarForCtrl[0][STATE_rlX]);
   float errY = -(posY - rlVarForCtrl[0][STATE_rlY]);
+  // calculate the proportional part for the velocity
   float pid_vx = rlPIDp * errX;
   float pid_vy = rlPIDp * errY;
+  //calculate the derivative part
   float dx = (errX - PreErrX) / dt;
   float dy = (errY - PreErrY) / dt;
+  //store current error for derivative in the next loop
   PreErrX = errX;
   PreErrY = errY;
+  //calculate the derivative part and add it to the velocity
   pid_vx += rlPIDd * dx;
   pid_vy += rlPIDd * dy;
+  //add this loops error to form the integral part
   IntErrX += errX * dt;
   IntErrY += errY * dt;
+  //calculate the integral part by the constrained integral memory and add it to the velocity
   pid_vx += rlPIDi * constrain(IntErrX, -0.5, 0.5);
   pid_vy += rlPIDi * constrain(IntErrY, -0.5, 0.5);
+  //constrain the calculated velocity to safe maximum value
   pid_vx = constrain(pid_vx, -1.5f, 1.5f);
   pid_vy = constrain(pid_vy, -1.5f, 1.5f);
+  // change the setpoint and send it to the High Level Commander
   setHoverSetpoint(&setpoint, pid_vx, pid_vy, height, 0);
 }
 
+/* appMain function = starting point for applayer superloop
+ *
+ * Parameters:
+ * - Nothing
+ * Returns:
+ * - Nothing
+ *                                                                            */
 void appMain() {
   vTaskDelay(M2T(3000));
   DEBUG_PRINT("Waiting for activation ...\n");
