10c43bb8c084acd6e3e59073b6d593acdeff21ce
karlscholz <scholz.karl@hm.edu>
2022-11-26 23:00:14 +0100

    pseudocode

--- examples/demos/uwb_swarm_demo/src/relative_localization.h
+++ examples/demos/uwb_swarm_demo/src/relative_localization.h
@@ -2,14 +2,17 @@
 #define RELATIVELOCALIZATION_H_
 #include "system.h"
 
+// enum for swarm.c array rlVarForCtrl and relative_localization.c array rlState
 typedef enum {
   STATE_rlX, STATE_rlY, STATE_rlYaw, STATE_DIM_rl
 } rlKalmanStateIdx;
 
+// idk, not used in project
 typedef enum {
   INPUT_vxi, INPUT_vyi, INPUT_ri, INPUT_vxj, INPUT_vyj, INPUT_rj, INPUT_DIM
 } rlKalmanInputIdx;
 
+// array with size NUM_UWB 
 typedef struct {
   float S[STATE_DIM_rl];
   float P[STATE_DIM_rl][STATE_DIM_rl];
