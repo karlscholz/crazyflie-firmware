ee7fa3856e979a681dcd88033c0dd3d459b58dfd
karlscholz <scholz.karl@hm.edu>
2022-12-23 23:39:36 +0100

    added diamond formation

--- examples/demos/uwb_swarm_demo/src/swarm.c
+++ examples/demos/uwb_swarm_demo/src/swarm.c
@@ -141,13 +141,33 @@ void appMain() {
 
       // after 30s, atomic pattern flight
       if (timeInAir >= 30000) {
-          float radius = (float)myId * 0.5f;
-          float timeInSecond = (float)timeInAir / configTICK_RATE_HZ;
-          float rlPosXofMeIn0 = radius * cosf(timeInSecond);
-          float rlPosYofMeIn0 = radius * sinf(timeInSecond);
-          desireX = -cosf(rlVarForCtrl[0][STATE_rlYaw]) * rlPosXofMeIn0 + sinf(rlVarForCtrl[0][STATE_rlYaw]) * rlPosYofMeIn0;
-          desireY = -sinf(rlVarForCtrl[0][STATE_rlYaw]) * rlPosXofMeIn0 - cosf(rlVarForCtrl[0][STATE_rlYaw]) * rlPosYofMeIn0;
-          moveWithLeaderAsOrigin(desireX, desireY);
+        float rlPosXofMeIn0;
+        float rlPosYofMeIn0;
+        float xydistance = 1.0f;
+        switch(myId){
+          case 1: // left
+            rlPosXofMeIn0 = -xydistance;
+            rlPosYofMeIn0 = xydistance;
+            break;
+          case 2: // right
+            rlPosXofMeIn0 = -xydistance;
+            rlPosYofMeIn0 = -xydistance;
+            break;
+          case 3: // back
+            rlPosXofMeIn0 = -2*xydistance;
+            rlPosYofMeIn0 = 0;
+            break;
+          default:
+            DEBUG_PRINT("myId is not in the range of 0-3\n");
+            desireY = rlVarForCtrl[0][STATE_rlX];
+            desireX = rlVarForCtrl[0][STATE_rlY];
+            moveWithLeaderAsOrigin(desireX, desireY);
+            continue;
+        }
+        // coordinate transformation from mine (rlVarForCtrl) to leaders coord system
+        desireX = -cosf(rlVarForCtrl[0][STATE_rlYaw]) * rlPosXofMeIn0 + sinf(rlVarForCtrl[0][STATE_rlYaw]) * rlPosYofMeIn0;
+        desireY = -sinf(rlVarForCtrl[0][STATE_rlYaw]) * rlPosXofMeIn0 - cosf(rlVarForCtrl[0][STATE_rlYaw]) * rlPosYofMeIn0;
+        moveWithLeaderAsOrigin(desireX, desireY);
       }
 
     } else {
