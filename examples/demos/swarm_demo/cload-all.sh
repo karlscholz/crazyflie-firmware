bff8215b28356871599fddffb782002fdef98399
karlscholz <scholz.karl@hm.edu>
2023-03-01 10:32:48 +0100

    fixed cload-all.sh

--- examples/demos/swarm_demo/cload-all.sh
+++ examples/demos/swarm_demo/cload-all.sh
@@ -1,20 +1,51 @@
 #!/usr/bin/env bash
 echo "Flasing 1"
-python3 -m cfloader flash cf2.bin stm32-fw -w radio://0/10/2M/E7E7E7E701
+python.exe -m cfloader flash build/cf2.bin stm32-fw -w radio://0/80/2M/E7E7E7E701
 echo "Flasing 2"
-python3 -m cfloader flash cf2.bin stm32-fw -w radio://0/10/2M/E7E7E7E702
+python.exe -m cfloader flash build/cf2.bin stm32-fw -w radio://0/80/2M/E7E7E7E702
 echo "Flasing 3"
-python3 -m cfloader flash cf2.bin stm32-fw -w radio://0/10/2M/E7E7E7E703
+python.exe -m cfloader flash build/cf2.bin stm32-fw -w radio://0/80/2M/E7E7E7E703
 echo "Flasing 4"
-python3 -m cfloader flash cf2.bin stm32-fw -w radio://0/10/2M/E7E7E7E704
+python.exe -m cfloader flash build/cf2.bin stm32-fw -w radio://0/80/2M/E7E7E7E704
 echo "Flasing 5"
-python3 -m cfloader flash cf2.bin stm32-fw -w radio://0/10/2M/E7E7E7E705
+python.exe -m cfloader flash build/cf2.bin stm32-fw -w radio://0/80/2M/E7E7E7E705
 echo "Flasing 6"
-python3 -m cfloader flash cf2.bin stm32-fw -w radio://0/10/2M/E7E7E7E706
+python.exe -m cfloader flash build/cf2.bin stm32-fw -w radio://0/80/2M/E7E7E7E706
 echo "Flasing 7"
-python3 -m cfloader flash cf2.bin stm32-fw -w radio://0/10/2M/E7E7E7E707
+python.exe -m cfloader flash build/cf2.bin stm32-fw -w radio://0/80/2M/E7E7E7E707
 echo "Flasing 8"
-python3 -m cfloader flash cf2.bin stm32-fw -w radio://0/10/2M/E7E7E7E708
+python.exe -m cfloader flash build/cf2.bin stm32-fw -w radio://0/80/2M/E7E7E7E708
 echo "Flasing 9"
-python3 -m cfloader flash cf2.bin stm32-fw -w radio://0/10/2M/E7E7E7E709
-
+python.exe -m cfloader flash build/cf2.bin stm32-fw -w radio://0/80/2M/E7E7E7E709
+echo "Flasing 0A"
+python.exe -m cfloader flash build/cf2.bin stm32-fw -w radio://0/80/2M/E7E7E7E70A
+echo "Flasing 0B"
+python.exe -m cfloader flash build/cf2.bin stm32-fw -w radio://0/80/2M/E7E7E7E70B
+echo "Flasing 0C"
+python.exe -m cfloader flash build/cf2.bin stm32-fw -w radio://0/80/2M/E7E7E7E70C
+echo "Flasing 0D"
+python.exe -m cfloader flash build/cf2.bin stm32-fw -w radio://0/80/2M/E7E7E7E70D
+echo "Flasing 0E"
+python.exe -m cfloader flash build/cf2.bin stm32-fw -w radio://0/80/2M/E7E7E7E70E
+echo "Flasing 0F"
+python.exe -m cfloader flash build/cf2.bin stm32-fw -w radio://0/80/2M/E7E7E7E70F
+echo "Flasing 10"
+python.exe -m cfloader flash build/cf2.bin stm32-fw -w radio://0/80/2M/E7E7E7E710
+echo "Flasing 11"
+python.exe -m cfloader flash build/cf2.bin stm32-fw -w radio://0/80/2M/E7E7E7E711
+echo "Flasing 12"
+python.exe -m cfloader flash build/cf2.bin stm32-fw -w radio://0/80/2M/E7E7E7E712
+echo "Flasing 13"
+python.exe -m cfloader flash build/cf2.bin stm32-fw -w radio://0/80/2M/E7E7E7E713
+echo "Flasing 14"
+python.exe -m cfloader flash build/cf2.bin stm32-fw -w radio://0/80/2M/E7E7E7E714
+echo "Flasing 15"
+python.exe -m cfloader flash build/cf2.bin stm32-fw -w radio://0/80/2M/E7E7E7E715
+echo "Flasing 16"
+python.exe -m cfloader flash build/cf2.bin stm32-fw -w radio://0/80/2M/E7E7E7E716
+echo "Flasing 17"
+python.exe -m cfloader flash build/cf2.bin stm32-fw -w radio://0/80/2M/E7E7E7E717
+echo "Flasing 18"
+python.exe -m cfloader flash build/cf2.bin stm32-fw -w radio://0/80/2M/E7E7E7E718
+echo "Flasing 19"
+python.exe -m cfloader flash build/cf2.bin stm32-fw -w radio://0/80/2M/E7E7E7E719
